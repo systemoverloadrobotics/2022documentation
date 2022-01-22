c5dadf0dffc8e64d6ca1702c81d64f7a146f6eca
OneLemonyBoi <50998649+OneLemonyBoi@users.noreply.github.com>
2022-01-21 20:04:34 -0800

    Config3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'javasphinx'
+    'sphinxext.remoteliteralinclude'
 ]
 
 intersphinx_mapping = {
